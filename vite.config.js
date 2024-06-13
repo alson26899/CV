9fa1bdab6e8d7e2201cd01912d0f0e8afc89ad00
Alson <alson.a@disprz.com>
2024-06-13 18:31:56 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // vite.config.js
-export default {};
+export default {
+  // ...
+  base: '/CV/', // Replace with your repository name
+  // ...
+};
